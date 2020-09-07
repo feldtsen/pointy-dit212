f41400ad102627d9fd3622764d531c4894a12933
feldtsen <feldtsen@gmail.com>
2020-09-07 18:11:08 +0200

    simple canvas setup

---
+++ project/src/main/java/game/controller/GameWindowController.java
@@ -0,0 +1,22 @@
+package game.controller;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import game.App;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+public class GameWindowController {
+
+    @FXML
+    private Canvas board;
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        //App.setRoot("secondary");
+        GraphicsContext gc = board.getGraphicsContext2D();
+        gc.setFill(Color.BLUE);
+        gc.fillRect(100, 100, 100, 100);
+    }
+}
