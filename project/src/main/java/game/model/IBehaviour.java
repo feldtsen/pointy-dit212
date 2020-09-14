baff1267920639b438f73a883a9d181df74a0c5a
Erik-Magnusson <Erik-Magnusson@users.noreply.github.com>
2020-09-14 14:06:57 +0100

    Added interfaces IAbility, IBehaviour

---
+++ project/src/main/java/game/model/IBehaviour.java
@@ -0,0 +1,10 @@
+package game.model;
+
+public interface IBehaviour {
+
+    /**
+    * Applies the behaviour of one entity to another entity.
+    * @param subject the entity which performs the behaviour, object the entitiy which the behaviour is being performed on.
+     */
+    boolean apply(IEntity subject, IEntity object);
+}
