9539e7a49b2f68704d8de7d39dcddf71de8cd845
feldtsen <feldtsen@gmail.com>
2020-09-14 23:40:52 +0200

    testing user inputs, implemented temporary moving logic, added styles folder containing css for ui elements

--- project/src/main/java/game/App.java
+++ project/src/main/java/game/App.java
@@ -1,9 +1,15 @@
 package game;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,12 +20,17 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
+    @FXML
+    private static Canvas canvas;
 
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("gamewindow"));
         stage.setScene(scene);
+
+
         stage.show();
+
     }
 
     public static void setRoot(String fxml) throws IOException {
--- project/src/main/java/game/controller/GameWindowController.java
+++ project/src/main/java/game/controller/GameWindowController.java
@@ -1,23 +1,148 @@
 package game.controller;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import game.model.entity.IEntity;
+import game.model.entity.movable.MovableEntity;
+import javafx.animation.AnimationTimer;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import game.App;
+import javafx.geometry.Point2D;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 
+import javax.crypto.Cipher;
+
 public class GameWindowController {
+
+    private class DummyPlayer extends MovableEntity {
+        public DummyPlayer(){
+            super(new Point2D(10, 10), 50, 5, 5);
+        }
+
+        public void moveRight (){
+            this.position = new Point2D(this.position.getX() + 5, this.position.getY());
+        }
+        public void moveLeft (){
+            this.position = new Point2D(this.position.getX() - 5, this.position.getY());
+        }
+        public void moveDown (){
+            this.position = new Point2D(this.position.getX(), this.position.getY() + 5);
+        }
+        public void moveUp (){
+            this.position = new Point2D(this.position.getX(), this.position.getY() - 5);
+        }
+
+        @Override
+        public boolean handleCollision(IEntity entity) {
+            return false;
+        }
+    }
 
     @FXML
     private Canvas canvas;
 
+    private class CurrentDirection {
+        private ArrayList<Boolean> directions = new ArrayList<>();
+
+        public CurrentDirection() {
+            directions.add(false);
+            directions.add(false);
+            directions.add(false);
+            directions.add(false);
+        }
+
+        public void register(KeyEvent e) {
+            switch (e.getCode()) {
+                case W:
+                    directions.set(0, true);
+                    break;
+                case A:
+                    directions.set(1, true);
+                    break;
+                case S:
+                    directions.set(2, true);
+                    break;
+                case D:
+                    directions.set(3, true);
+                    break;
+
+            }
+
+        }
+
+        public void unregister(KeyEvent e){
+            switch (e.getCode()) {
+                case W:
+                    directions.set(0, false);
+                    break;
+                case A:
+                    directions.set(1, false);
+                    break;
+                case S:
+                    directions.set(2, false);
+                    break;
+                case D:
+                    directions.set(3, false);
+                    break;
+            }
+
+        }
+
+        public ArrayList<Boolean> getCurrentDirections(){
+            return directions;
+        }
+
+    }
     @FXML
-    private void drawRectangle() throws IOException {
-        //App.setRoot("secondary");
+    Pane gamePane;
+
+    @FXML
+    private void startGame() throws IOException {
+        final long startNanoTime = System.nanoTime();
+
         GraphicsContext gc = canvas.getGraphicsContext2D();
-        gc.setFill(Color.BLUE);
-        gc.fillRect(100, 100, 100, 100);
+
+        DummyPlayer player = new DummyPlayer();
+
+        CurrentDirection currentDirection = new CurrentDirection();
+
+        gamePane.setOnKeyPressed(currentDirection::register);
+        gamePane.setOnKeyReleased(currentDirection::unregister);
+
+        new AnimationTimer()
+        {
+            public void handle(long currentNanoTime)
+            {
+                double t = (currentNanoTime - startNanoTime) / 1000000000.0;
+                gc.clearRect(0, 0, 1200, 800);
+                gc.setFill(Color.rgb(50, 50, 50));
+                gc.fillRect(0, 0, 1200, 800);
+                gc.setFill(Color.WHITE);
+                gc.fillOval(player.getPosition().getX(), player.getPosition().getY(), player.getWidth(), player.getHeight());
+
+                if(currentDirection.getCurrentDirections().get(0)) {
+                    player.moveUp();
+                }
+                if(currentDirection.getCurrentDirections().get(1)) {
+                    player.moveLeft();
+                }
+                if(currentDirection.getCurrentDirections().get(2)) {
+                    player.moveDown();
+                }
+
+                if(currentDirection.getCurrentDirections().get(3)) {
+                    player.moveRight();
+                }
+
+            }
+        }.start();
     }
 }
