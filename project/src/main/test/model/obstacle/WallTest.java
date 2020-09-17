815f472d2558fbcd4111af02e821d362179c3ac4
Sgenne <simon.genne@gmail.com>
2020-09-17 11:39:10 +0200

    Changed WallTest

--- project/src/main/test/model/obstacle/WallTest.java
+++ project/src/main/test/model/obstacle/WallTest.java
@@ -2,6 +2,7 @@ package model.obstacle;
 
 import game.model.entity.Entity;
 import game.model.entity.IEntity;
+import game.model.entity.movable.MovableEntity;
 import game.model.entity.obstacle.Wall;
 import javafx.geometry.Point2D;
 import org.junit.Test;
@@ -11,21 +12,13 @@ import static org.junit.Assert.*;
 public class WallTest {
     private Entity wall  = new Wall(new Point2D(50, 50), 50, 50);
 
-    /* Helper method required for initializing an anonymous instance of the abstract class */
-    public IEntity createEntity(Point2D position, double radius) {
-        return new Entity(position, radius) {
-            @Override
-            public void update(double delta) {
-            }
-        };
-    }
 
     @Test
     public void testCheckCollisionNoCollision() {
-        IEntity entity0 = createEntity(new Point2D(100, 100),10);  /* Bottom right of wall. */
-        IEntity entity1 = createEntity(new Point2D(40, 40), 10);  /* Top left of wall. */
-        IEntity entity2 = createEntity(new Point2D(100, 40), 10); /* Top right of wall. */
-        IEntity entity3 = createEntity(new Point2D(40, 100), 10); /* Bottom left of wall */
+        IEntity entity0 = new MovableEntity(new Point2D(100, 100), 10, 3, 3) {};  /* Bottom right of wall. */
+        IEntity entity1 = new MovableEntity(new Point2D(40, 40), 10, 3, 3){};  /* Top left of wall. */
+        IEntity entity2 = new MovableEntity(new Point2D(100, 40), 10, 3, 3){}; /* Top right of wall. */
+        IEntity entity3 = new MovableEntity(new Point2D(40, 100), 10, 3, 3) {}; /* Bottom left of wall */
 
 
         assertFalse(wall.checkCollision(entity0) || wall.checkCollision(entity1) ||
@@ -34,10 +27,10 @@ public class WallTest {
 
     @Test
     public void testCheckCollisionCollision() {
-        IEntity entity0 = createEntity(new Point2D(99, 99), 10); /* Bottom right of wall */
-        IEntity entity1 = createEntity(new Point2D(41, 41), 10); /* Top left of wall */
-        IEntity entity2 = createEntity(new Point2D(99, 41), 10); /* Bottom left of wall */
-        IEntity entity3 = createEntity(new Point2D(99, 99), 10); /* Bottom right of wall */
+        IEntity entity0 = new MovableEntity(new Point2D(99, 99), 10, 3, 3) {}; /* Bottom right of wall */
+        IEntity entity1 = new MovableEntity(new Point2D(41, 41), 10, 3, 3) {}; /* Top left of wall */
+        IEntity entity2 = new MovableEntity(new Point2D(99, 41), 10, 3, 3) {}; /* Bottom left of wall */
+        IEntity entity3 = new MovableEntity(new Point2D(99, 99), 10, 3, 3) {}; /* Bottom right of wall */
 
         assertTrue(wall.checkCollision(entity0) && wall.checkCollision(entity1) &&
                              wall.checkCollision(entity2) && wall.checkCollision(entity3));
