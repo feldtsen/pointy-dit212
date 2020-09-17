a7d3a217560940e85fdbc4642984b227b970601c
Mattoom <kall0795@gmail.com>
2020-09-17 13:34:25 +0200

    remove getShape

--- project/src/main/java/game/model/entity/IEntity.java
+++ project/src/main/java/game/model/entity/IEntity.java
@@ -6,7 +6,6 @@ import game.model.shape2d.IShape2D;
 public interface IEntity extends IPositionable {
     double getWidth();
     double getHeight();
-    IShape2D getShape();
     boolean checkCollision(IEntity entity);
 
     void update(double delta);
