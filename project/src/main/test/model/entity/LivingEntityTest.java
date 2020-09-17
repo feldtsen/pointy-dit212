43ee86cd985893abea4a7eb164beef945550c3cf
Erik-Magnusson <Erik-Magnusson@users.noreply.github.com>
2020-09-17 15:01:00 +0100

    adjusted LivingEntityTest to new constructor

--- project/src/main/test/model/entity/LivingEntityTest.java
+++ project/src/main/test/model/entity/LivingEntityTest.java
@@ -2,6 +2,8 @@ package model.entity;
 
 import game.model.ILiving;
 import game.model.entity.movable.LivingEntity;
+import game.model.shape2d.Circle;
+import game.model.shape2d.IShape2D;
 import javafx.geometry.Point2D;
 import org.junit.Test;
 
@@ -10,8 +12,8 @@ import static org.junit.Assert.assertFalse;
 
 public class LivingEntityTest {
     /* Helper method required for initializing an anonymous instance of the abstract class */
-    public ILiving createLivingEntity(Point2D position, double radius, double maxForce, double maxSpeed, int hitPoints) {
-        return new LivingEntity(position, radius, maxForce, maxSpeed, hitPoints) {
+    public ILiving createLivingEntity(Point2D position, double maxForce, double maxSpeed, int hitPoints, IShape2D shape) {
+        return new LivingEntity(position, maxForce, maxSpeed, hitPoints, shape) {
 
             @Override
             public void update(double delta) {
@@ -21,8 +23,8 @@ public class LivingEntityTest {
 
     @Test
     public void testAlive(){
-        ILiving e1 = createLivingEntity(new Point2D(0,0), 10,0,0,10);
-        ILiving e2 = createLivingEntity(new Point2D(0,0), 10,0,0,0);
+        ILiving e1 = createLivingEntity(new Point2D(0,0), 10,0,10, new Circle(10));
+        ILiving e2 = createLivingEntity(new Point2D(0, 0),10,0,0,new Circle(10));
         assert(e1.isAlive());
         assertFalse(e2.isAlive());
 
