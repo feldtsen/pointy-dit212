843df401de3721c4311c9ceee689e8669c086460
Mattoom <kall0795@gmail.com>
2020-09-17 12:46:51 +0200

    added getShape

--- project/src/main/java/game/model/entity/IEntity.java
+++ project/src/main/java/game/model/entity/IEntity.java
@@ -2,10 +2,12 @@ package game.model.entity;
 
 import game.model.IPositionable;
 import javafx.geometry.Point2D;
+import javafx.scene.shape.Shape;
 
 public interface IEntity extends IPositionable {
     double getWidth();
     double getHeight();
+    Shape getShape();
     boolean checkCollision(IEntity entity);
 
     void update(double delta);
