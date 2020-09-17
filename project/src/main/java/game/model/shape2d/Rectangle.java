03974e98287a067ed0f42c8ae864ce7ebfdd014f
Sgenne <simon.genne@gmail.com>
2020-09-17 14:12:50 +0200

    Rectangle implements IRectangle

--- project/src/main/java/game/model/shape2d/Rectangle.java
+++ project/src/main/java/game/model/shape2d/Rectangle.java
@@ -1,11 +1,32 @@
 package game.model.shape2d;
 
-public class Rectangle implements IShape2D{
+public class Rectangle implements IShape2D, IRectangle{
     private double width;
     private double height;
+    private double rotation;
 
-    public Rectangle(double width, double height) {
+    public Rectangle(double width, double height, double rotation) {
         this.height = height;
         this.width = width;
+        this.rotation = 0;
+    }
+
+    @Override
+    public double getWidth() {
+        return width;
+    }
+
+    @Override
+    public double getHeight() {
+        return height;
+    }
+
+    @Override
+    public double getRotation() {
+        return rotation;
+    }
+
+    public void setRotation(double rotation)  {
+        this.rotation = rotation;
     }
 }
