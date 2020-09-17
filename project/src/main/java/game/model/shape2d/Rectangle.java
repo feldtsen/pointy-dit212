a38f9c4d53f27f4f9aa18a36b35d49bf7ab21261
Sgenne <simon.genne@gmail.com>
2020-09-17 13:56:37 +0200

    Added Rectangle

---
+++ project/src/main/java/game/model/shape2d/Rectangle.java
@@ -0,0 +1,11 @@
+package game.model.shape2d;
+
+public class Rectangle implements IShape2D{
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.height = height;
+        this.width = width;
+    }
+}
