00bcc58d0bd5d77b495c206135a49e2c0d7993b8
Sgenne <simon.genne@gmail.com>
2020-09-18 15:13:02 +0200

    Added tests for testCollision rectangles

--- project/src/main/test/model/shape/ShapesTest.java
+++ project/src/main/test/model/shape/ShapesTest.java
@@ -1,9 +1,13 @@
 package model.shape;
 
 import game.model.shape2d.Circle;
+import game.model.shape2d.Rectangle;
 import game.model.shape2d.Shapes;
 import javafx.geometry.Point2D;
 import org.junit.Test;
+
+import java.awt.*;
+
 import static org.junit.Assert.*;
 public class ShapesTest {
 
@@ -38,6 +42,28 @@ public class ShapesTest {
 
         assertFalse(Shapes.testCollision(c1,p1,c2,p2));
 
+    }
+
+    @Test
+    public void testRectangleCollision() {
+        Rectangle r1 = new Rectangle(1, 1, 0);
+        Rectangle r2 = new Rectangle(1, 1, 0);
+
+        Point2D p1 = new Point2D(0, 0);
+        Point2D p2 = new Point2D(1, 1);
+
+        assertTrue(Shapes.testCollision(r1, p1, r2, p2));
+    }
+
+    @Test
+    public void testRectangleNoCollision() {
+        Rectangle r1 = new Rectangle(1, 1, 0);
+        Rectangle r2 = new Rectangle(1, 1, Math.PI/6);
+
+        Point2D p1 = new Point2D(0, 0);
+        Point2D p2 = new Point2D(1, 1);
+
+        assertFalse(Shapes.testCollision(r1, p1, r2, p2));
     }
 
 }
