a8bf6c653bb863f093997645ae205c8604f55685
Sgenne <simon.genne@gmail.com>
2020-09-18 10:35:41 +0200

    Added helper method getAxes to Shapes

--- project/src/main/java/game/model/shape2d/Shapes.java
+++ project/src/main/java/game/model/shape2d/Shapes.java
@@ -15,21 +15,16 @@ public class Shapes {
     }
 
     public static boolean testCollision(IRectangle r1, Point2D r1Position, IRectangle r2 , Point2D r2Position) {
-        Point2D[] r1Corners = getCornerCoordinates(r1, r1Position);
-        Point2D[] r2Corners = getCornerCoordinates(r2, r2Position);
+        Point2D[] r1Corners = getCornerCoordinates(r1, r1Position); // Corners of first rectangle
+        Point2D[] r2Corners = getCornerCoordinates(r2, r2Position); // Corners of second rectangle
+
+
 
 
         return false;
     }
 
-    /*
-    Returns the coordinates of the corners of the given rectangle.
-    The elements in the array represent the following corners:
-    0 - top left
-    1 - bottom left
-    2 - top right
-    3 - bottom left
-     */
+    // Returns the coordinates of the corners of the given rectangle.
     private static Point2D[] getCornerCoordinates(IRectangle rectangle, Point2D position) {
         Point2D[] corners = new Point2D[4];
 
@@ -39,6 +34,21 @@ public class Shapes {
         corners[3] = position.add(new Point2D(rectangle.getWidth()/2, -rectangle.getHeight()/2)); //Bottom right
 
         return corners;
+    }
+
+    // Takes an array of a rectangles corners and returns an array of axes parallel to the edges of the rectangle.
+    private static Point2D[] getRectangleAxes(Point2D[] corners) {
+        Point2D[] axes = new Point2D[corners.length];
+
+        for (int i = 0; i < axes.length - 1; i++) {
+            Point2D vector = corners[i].subtract(corners[i + 1]).normalize();
+            // Get normal???
+            axes[i] = vector;
+        }
+        Point2D vector = corners[axes.length - 1].subtract(corners[0]).normalize();
+        axes[axes.length - 1] = vector;
+
+        return axes;
     }
 
 }
