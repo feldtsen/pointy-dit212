a8949c1879a85414d8cca8b7e30727c46854d7e3
Mattoom <kall0795@gmail.com>
2020-09-21 15:38:34 +0200

    removed DeathTouch and added strength to enemies

--- project/src/main/java/game/model/entity/enemy/Enemy.java
+++ project/src/main/java/game/model/entity/enemy/Enemy.java
@@ -11,16 +11,26 @@ import javafx.geometry.Point2D;
 
 public class Enemy extends LivingEntity<ICircle> implements IEnemy<ICircle> {
 
+
+    private final int strength;
     private IEntity<?> target;
     private IMovementBehaviour movementBehaviour;
     private IAbilityBehaviour abilityBehaviour;
 
-
-    public Enemy(Point2D position, double radius, double maxForce, double maxSpeed, int hitPoints, IAbilityBehaviour abilityBehaviour, IMovementBehaviour movementBehaviour, IEntity<?> target) {
+    public Enemy(Point2D position, double radius, double maxForce, double maxSpeed, int hitPoints, IAbilityBehaviour abilityBehaviour, IMovementBehaviour movementBehaviour, IEntity<?> target){
         super(position, maxForce, maxSpeed, hitPoints, new Circle(radius));
         this.abilityBehaviour = abilityBehaviour;
         this.movementBehaviour = movementBehaviour;
         this.target = target;
+        this.strength = 1;
+    }
+
+    public Enemy(Point2D position, double radius, double maxForce, double maxSpeed, int hitPoints, IAbilityBehaviour abilityBehaviour, IMovementBehaviour movementBehaviour, IEntity<?> target, int strength) {
+        super(position, maxForce, maxSpeed, hitPoints, new Circle(radius));
+        this.abilityBehaviour = abilityBehaviour;
+        this.movementBehaviour = movementBehaviour;
+        this.target = target;
+        this.strength = strength;
     }
 
     @Override
