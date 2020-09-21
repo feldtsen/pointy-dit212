630e87cd1ec6f2603dfae2deb1e98a0769b1a8b7
Mattoom <kall0795@gmail.com>
2020-09-21 12:38:54 +0200

    removed unneeded code

--- project/src/main/java/game/model/entity/obstacle/Wall.java
+++ project/src/main/java/game/model/entity/obstacle/Wall.java
@@ -11,35 +11,6 @@ public class Wall extends Entity<Rectangle> implements IObstacle<Rectangle> {
         super(position, new Rectangle(width, height,0));
     }
 
-    @Override
-    /* Checks if an Entity has collided with the wall. */
-    public boolean checkCollision(IEntity<?> entity) {
-        /* Size and coordinates of the entity to check for collision. */
-        //double entityWidth = entity.getWidth();
-        //double entityHeight = entity.getHeight();
-        double entityX = entity.getPosition().getX();
-        double entityY = entity.getPosition().getY();
-
-        /* Coordinates of wall. */
-        double wallX = super.getPosition().getX();
-        double wallY = super.getPosition().getY();
-
-        /* Booleans indicating if the entity overlaps with the wall in x- and y-axis. */
-        boolean xOverlap = false;
-        boolean yOverlap = false;
-
-        //if (entityX < wallX + width && entityX + entityWidth > wallX) {
-        //    xOverlap = true;
-        //}
-
-        //if (entityY < wallY + height && entityY + entityHeight > wallY) {
-        //    yOverlap = true;
-        //}
-
-        /* True if there is an overlap both in x- and y-axis. */
-        //return xOverlap && yOverlap;
-        return false;
-    }
 
     @Override
     public void update(double delta) {
