11678cee6f5fc1a1c68125a370c25a7f6dd7d03d
palmdrop <anton@exlex.se>
2020-09-21 09:28:38 +0200

    made simple enemytest

---
+++ project/src/main/test/model/entity/EnemyTest.java
@@ -0,0 +1,33 @@
+package model.entity;
+
+import game.model.entity.IEntity;
+import game.model.entity.enemy.Enemy;
+import game.model.entity.enemy.IEnemy;
+import game.model.player.Player;
+import javafx.geometry.Point2D;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class EnemyTest {
+    private Enemy enemy;
+    private IEntity<?> target;
+
+    @Before
+    public void setup() {
+        enemy = new Enemy(new Point2D(0,0), 10, 2, 2, 1, null, null, null);
+        target = new Player(new Point2D(0,0), 10, 2, 2);
+    }
+
+    @Test
+    public void testSetTarget() {
+        assertTrue(enemy.setTarget(target));
+    }
+
+    @Test
+    public void testSetSelfTarget() {
+        assertFalse(enemy.setTarget(enemy));
+    }
+
+
+}
