c75affd1ed198652656717866cd9d8256e3d1bd4
Erik-Magnusson <Erik-Magnusson@users.noreply.github.com>
2020-09-23 14:49:50 +0100

    added obstacles

--- project/src/main/java/game/model/entity/obstacle/Wall.java
+++ project/src/main/java/game/model/entity/obstacle/Wall.java
@@ -1,12 +1,9 @@
 package game.model.entity.obstacle;
-
 import game.model.entity.Entity;
-import game.model.entity.IEntity;
-import game.model.shape2d.IShape2D;
 import game.model.shape2d.Rectangle;
 import javafx.geometry.Point2D;
 
-public class Wall extends Entity<Rectangle> implements IObstacle<Rectangle> {
+public class Wall extends Entity<Rectangle> implements IObstacle<Rectangle>{
 
     public Wall(Point2D position, double width, double height) {
         super(position, new Rectangle(width, height,0));
