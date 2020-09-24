c98c5800754e4d18dd5bd4c7d31b9d891bc3adde
Sgenne <simon.genne@gmail.com>
2020-09-24 15:38:03 +0200

    Check if target is alive before adding bullet

--- project/src/main/java/game/model/ability/ShootBullet.java
+++ project/src/main/java/game/model/ability/ShootBullet.java
@@ -1,11 +1,16 @@
 package game.model.ability;
 
+import game.model.ILiving;
 import game.model.IMovable;
 import game.model.ability.action.IAbilityAction;
+import game.model.entity.Entity;
+import game.model.entity.IEntity;
+import game.model.entity.movable.LivingEntity;
 import game.model.entity.projectile.Bullet;
 import game.model.entity.projectile.IProjectile;
 import game.model.entity.projectile.Projectile;
 import game.model.level.ILevel;
+import game.model.shape2d.Circle;
 import game.util.Utils;
 import javafx.geometry.Point2D;
 
@@ -13,7 +18,7 @@ import java.util.List;
 
 public class ShootBullet extends Ability {
 
-    public ShootBullet(IMovable user, IMovable target, long cooldown, double bulletRadius, double maxForce, double maxSpeed, int damage) {
+    public ShootBullet(Entity<Circle> user, LivingEntity<Circle> target, long cooldown, double bulletRadius, double maxForce, double maxSpeed, int damage) {
         super(cooldown, new IAbilityAction() {
             @Override
             public double getRadius() {
@@ -30,6 +35,9 @@ public class ShootBullet extends Ability {
             // projectiles. The bullet is fired from the position of the user and fired towards the position of the
             // target.
             public void apply(ILevel level, double timePassed) {
+                // Only add a new projectile if the target is alive.
+                if (!target.isAlive()) return;
+
                 // Gets velocity by subtracting targets position from user and limiting to maxSpeed.
                 Point2D bulletVelocity = target.getPosition().subtract(user.getPosition());
                 bulletVelocity = Utils.limit(bulletVelocity, maxSpeed);
@@ -43,5 +51,4 @@ public class ShootBullet extends Ability {
             }
         });
     }
-
 }
