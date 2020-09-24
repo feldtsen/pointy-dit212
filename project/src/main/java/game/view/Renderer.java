9de082fb36d7c03814a95bdb20dbea8a3b0296c3
feldtsen <feldtsen@gmail.com>
2020-09-24 14:40:50 +0200

    implemented test function for rectangle translatation

--- project/src/main/java/game/view/Renderer.java
+++ project/src/main/java/game/view/Renderer.java
@@ -3,6 +3,8 @@ package game.view;
 import game.model.entity.enemy.Enemy;
 import game.model.entity.player.Player;
 import game.model.level.ILevel;
+import game.model.shape2d.Rectangle;
+import javafx.geometry.Point2D;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
@@ -11,6 +13,8 @@ import java.util.HashMap;
 public class Renderer implements IRenderer{
     private final HashMap<Class<?>, Color> shapeColors = new HashMap<>();
     private final GraphicsContext graphicsContext;
+
+    private final Rectangle testRect = new Rectangle(40, 40, 0);
 
     public Renderer(GraphicsContext graphicsContext) {
         this.graphicsContext = graphicsContext;
@@ -29,7 +33,13 @@ public class Renderer implements IRenderer{
 
         for (Enemy enemy : level.getEnemies()) {
             RendererUtils.drawShape(graphicsContext, shapeColors.get(enemy.getClass()), enemy.getShape(), enemy.getPosition());
+
         }
+
+
+        //TODO: remove this (used for testing)
+        RendererUtils.translate(graphicsContext, testRect);
+        testRect.setRotation(testRect.getRotation() + 1);
     }
 
 }
