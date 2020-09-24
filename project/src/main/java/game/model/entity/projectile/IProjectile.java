53696d65d7a995e52192a672f6d5255b29c8de58
Sgenne <simon.genne@gmail.com>
2020-09-24 21:49:08 +0200

    Made IProjectile extend IMovable

--- project/src/main/java/game/model/entity/projectile/IProjectile.java
+++ project/src/main/java/game/model/entity/projectile/IProjectile.java
@@ -1,9 +1,10 @@
 package game.model.entity.projectile;
 
+import game.model.IMovable;
 import game.model.entity.IStrength;
 import game.model.shape2d.IShape2D;
 
-public interface IProjectile<T extends IShape2D> extends IStrength {
+public interface IProjectile<T extends IShape2D> extends IStrength, IMovable {
     boolean isDestroyed();
     void setDestroyed();
 }
