ff2b1cf2c6d945d5b08dcea1c2577ccd1a9adb68
Sgenne <simon.genne@gmail.com>
2020-09-24 21:46:37 +0200

    Added ShootBullet tests

--- project/src/main/test/model/ability/ShootBulletTest.java
+++ project/src/main/test/model/ability/ShootBulletTest.java
@@ -13,43 +13,31 @@ import game.model.entity.movable.MovableEntity;
 import game.model.entity.obstacle.IObstacle;
 import game.model.entity.player.Player;
 import game.model.entity.projectile.IProjectile;
+import game.model.entity.projectile.Projectile;
 import game.model.level.ILevel;
 import game.model.level.Level;
 import game.model.shape2d.Circle;
 import javafx.geometry.Point2D;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/*
-    TODO:
-        PROBLEM:
-         ShootBullet takes a target and user as constructor arguments. ShootBullet needs to be created after
-         user.
-         Enemy(user) needs to have an IAbilityBehaviour in Constructor -> needs Ability (ShootBullet).
-         Enemy needs to be created after ShootBullet.
-         Which should be created first and how???
-         Attempts:
-         Creating setters in ShootBullet doesn't fix constructor issue, since anonymous AbilityAction wouldn't
-         be affected.
-         Brainstorm solutions:
-         Give IAbilityBehaviour setters for Abilities so that an enemy can get created without needing to
-         initialize Abilities
-
- */
+import static org.junit.Assert.*;
 
 public class ShootBulletTest {
     Enemy user;
     Enemy target;
     ILevel level;
+    IAbilityBehaviour abilityBehaviour;
 
     //Create AbilityBehaviour with dummy apply method
     //Create enemy, add abilityBehaviour
     //Create ShootBullet and add to abilityBehaviour
     public void init() {
         // Creates abilityBehaviour without adding any abilities
-        IAbilityBehaviour abilityBehaviour = new AbilityBehaviour() {
+        abilityBehaviour = new AbilityBehaviour() {
             @Override
             // Dummy method. If there is an ability in list, use first one.
             public IAbilityAction apply(ILevel level) {
@@ -67,7 +55,33 @@ public class ShootBulletTest {
         List<IAbility> abilities = new ArrayList<>();
         abilities.add(0, new ShootBullet(user, target, 3, 0.1, 5, 5, 1));
         abilityBehaviour.setAbilities(abilities);
+
+        // Create new level with list for projectiles
+        level = new Level(null, new ArrayList<>(), null, null, 100, 100);
     }
 
+    @Test
+    // Test if using the ability leads to a projectile being added.
+    public void testShoot() {
+        init();
+        shoot();
 
+        assertTrue(level.getProjectiles().size() == 1);
+    }
+
+    /*
+    @Test
+    public void testBulletVelocity() {
+        init();
+        shoot();
+        <?> bullet = level.getProjectiles().get(0).;
+        assertTrue(bullet.getVelocity());
+    }
+    
+     */
+
+    private void shoot() {
+        IAbilityAction abilityAction = abilityBehaviour.apply(level);
+        abilityAction.apply(level, 0);
+    }
 }
