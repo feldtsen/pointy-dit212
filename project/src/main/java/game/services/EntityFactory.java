18279ebe77c05af305cadb737ceded72b5d60eae
Erik-Magnusson <Erik-Magnusson@users.noreply.github.com>
2020-09-29 15:10:16 +0100

    commented factory

--- project/src/main/java/game/services/EntityFactory.java
+++ project/src/main/java/game/services/EntityFactory.java
@@ -12,16 +12,19 @@ import javafx.geometry.Point2D;
 
 import java.util.List;
 
+
 public class EntityFactory {
 
     public static Player basicPlayer(double x, double y) {
         return new Player(new Point2D(x, y), 30, 2500, 1000,0);
     }
 
+    //Creates a player with a higher strength than basicPlayer.
     public static Player strongPlayer(double x, double y) {
         return new Player(new Point2D(x, y), 30, 2500, 1000,10);
     }
 
+    //Creates and enemy without abilities which only follows a target.
     public static Enemy basicEnemy(double x, double y, IEntity<?> target, int strength) {
         return new Enemy(new Point2D(x,y), 50, 1000, 1000, 1,null, new SeekingBehaviour(), target, strength);
     }
@@ -37,6 +40,7 @@ public class EntityFactory {
         return new Enemy(new Point2D(x,y), 50, 1000, 1000, 1,null, new SeekingBehaviour(), target, strength);
     }
 
+    //Creates an enemy with the ability of homing missiles.
     public static Enemy missileEnemy(double x, double y, IEntity<?> target, int strength) {
         //TODO
         return null;
