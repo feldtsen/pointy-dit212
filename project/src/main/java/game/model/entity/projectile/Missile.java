2836bcc32d559daebeeb43dbef1d169d0824d3a2
Sgenne <simon.genne@gmail.com>
2020-09-30 13:37:53 +0200

    Added homing behaviour to missile.

--- project/src/main/java/game/model/entity/projectile/Missile.java
+++ project/src/main/java/game/model/entity/projectile/Missile.java
@@ -4,17 +4,21 @@ import game.model.entity.IEntity;
 import game.model.shape2d.Circle;
 import game.model.shape2d.ICircle;
 import game.model.shape2d.IShape2D;
+import game.util.Utils;
 import javafx.geometry.Point2D;
 
-// Missiles are a hoaming projectile which steers towards its target.
+// Missiles are a homing projectile which steers towards its target.
 public class Missile extends Projectile<ICircle> {
 
-    private double steering; //How fast a missile turns towards the player
     private IEntity<?> target;
-    //TODO: add movement behavior
+    double minSpeed; // The minimum speed that the missile can have.
+    //TODO: add movement behavior. (Are movement behaviours needed?)
 
-    public Missile(Point2D position, double radius, double maxForce, double maxSpeed, int strength, Point2D velocity, IEntity<?> target) {
+    // The responsiveness of the missile to the movement of the target is dictated by the size of maxForce. The more
+    // responsive the missile should be, the larger maxForce it should have.
+    public Missile(Point2D position, double radius, double maxForce, double minSpeed, double maxSpeed, int strength, Point2D velocity, IEntity<?> target) {
         super(position, maxForce, maxSpeed, strength, velocity, new Circle(radius));
+        this.minSpeed = minSpeed;
         this.target = target;
     }
 
@@ -28,9 +32,18 @@ public class Missile extends Projectile<ICircle> {
         return target;
     }
 
-    //TODO: Use this to implement missile homing
     @Override
     public void update(double delta, double timeStep) {
+        if (target != null) {
+            // Create acceleration vector pointed towards target.
+            Point2D acceleration = target.getPosition().subtract(getPosition());
+
+            // Set acceleration to maxForce.
+            acceleration = Utils.setMagnitude(acceleration, getMaxForce());
+
+            // Apply acceleration to velocity and apply lowerLimit at minSpeed.
+            setVelocity(Utils.lowerLimit(getVelocity().add(acceleration), minSpeed));
+        }
         super.update(delta, timeStep);
     }
 }
