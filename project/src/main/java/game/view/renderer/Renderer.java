d541d6a2cb364384f990902edf6c39ef37872721
feldtsen <feldtsen@gmail.com>
2020-10-04 23:37:29 +0200

    more pleasant testing colors

--- project/src/main/java/game/view/renderer/Renderer.java
+++ project/src/main/java/game/view/renderer/Renderer.java
@@ -32,9 +32,9 @@ public class Renderer implements IRenderer {
         this.graphicsContext = graphicsContext;
 
         // Initialize different entity classes with different colors
-        colors.put(Player.class,          Color.rgb(200, 200, 200));
+        colors.put(Player.class,          Color.rgb(150, 150, 150));
         colors.put(Enemy.class,           Color.rgb(80, 80, 80));
-        colors.put(Bullet.class,          Color.rgb(100, 100, 100));
+        colors.put(Bullet.class,          Color.rgb(90, 90, 90));
         colors.put(GraphicsContext.class, Color.rgb(30,  30,  30 ));
     }
 
