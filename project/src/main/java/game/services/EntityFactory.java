0c6a708f896e7e5804e676b51c251f927874aa6a
Sgenne <simon.genne@gmail.com>
2020-10-06 13:29:19 +0200

    Added missileEnemy with difficulty

--- project/src/main/java/game/services/EntityFactory.java
+++ project/src/main/java/game/services/EntityFactory.java
@@ -2,7 +2,9 @@ package game.services;
 
 import game.controller.gameLoop.GameLoop;
 import game.model.ability.ShootBullet;
+import game.model.ability.ShootMissile;
 import game.model.behavior.ability.SingleAbilityBehavior;
+import game.model.behavior.movement.IMovementBehaviour;
 import game.model.behavior.movement.SeekingBehaviour;
 import game.model.entity.IEntity;
 import game.model.entity.enemy.Enemy;
@@ -12,11 +14,18 @@ import javafx.geometry.Point2D;
 
 
 public class EntityFactory {
-    private static final long BASEFREQUENCY = GameLoop.SECOND;
+    private static final long BASEBULLETFREQUENCY = GameLoop.SECOND;
     private static final int BASEENEMYSTRENGTH = 5;
     private static final double BASEBULLETRADIUS = 10;
-    private static final double BASEBULLETSPEED = 300 + 100;
+    private static final double BASEBULLETSPEED = 400;
     private static final int BASEBULLETSTRENGTH = 1;
+
+    private static final double BASEMISSILEMAXFORCE = 500;
+    private static final double BASEMISSILEMAXSPEED = 400;
+    private static final double BASEMISSILEMINSPEED = 200;
+    private static final int BASEMISSILESTRENGTH = 3;
+    private static final long BASEMISSILEFREQEUNCY = GameLoop.SECOND * 2;
+    private static final double BASEMISSILERADIUS = 10;
 
 
     public static Player basicPlayer(double x, double y) {
@@ -35,7 +44,9 @@ public class EntityFactory {
 
     //Creates an enemy with the ability of shooting bullets. The higher the given difficulty is, the higher the shooting frequency.
     public static Enemy bulletEnemy(double x, double y, IEntity<?> target, int difficulty) {
-        ShootBullet shootBullet = new ShootBullet((long ) (BASEFREQUENCY / (difficulty * 0.5)), BASEBULLETRADIUS, 0, BASEBULLETSPEED, BASEBULLETSTRENGTH);
+        if (difficulty < 1) throw new IllegalArgumentException();
+
+        ShootBullet shootBullet = new ShootBullet((long ) (BASEBULLETFREQUENCY / (difficulty * 0.5)), BASEBULLETRADIUS, 0, BASEBULLETSPEED, BASEBULLETSTRENGTH);
         Enemy enemy = basicEnemy(x, y, target, BASEENEMYSTRENGTH);
         enemy.setAbilityBehaviour(new SingleAbilityBehavior(shootBullet));
         return enemy;
@@ -45,9 +56,12 @@ public class EntityFactory {
         return new Enemy(new Point2D(x,y), 50, 1000, 1000, 1,null, new SeekingBehaviour(), target, strength);
     }
 
-    //Creates an enemy with the ability of homing missiles.
-    public static Enemy missileEnemy(double x, double y, IEntity<?> target, int strength) {
-        //TODO
-        return null;
+    //Creates an enemy with the ability of homing missiles. The given difficulty changes the max speed and responsiveness of the missiles.
+    public static Enemy missileEnemy(double x, double y, IEntity<?> target, int difficulty) {
+        IMovementBehaviour movementBehaviour = new SeekingBehaviour();
+        ShootMissile shootMissile = new ShootMissile(BASEMISSILEFREQEUNCY, BASEMISSILERADIUS, BASEMISSILEMAXFORCE + 300 * difficulty, BASEMISSILEMAXSPEED + 25 * difficulty , BASEMISSILEMINSPEED, BASEMISSILESTRENGTH, movementBehaviour);
+        Enemy enemy = basicEnemy(x, y, target, BASEENEMYSTRENGTH);
+        enemy.setAbilityBehaviour(new SingleAbilityBehavior(shootMissile));
+        return enemy;
     }
 }
