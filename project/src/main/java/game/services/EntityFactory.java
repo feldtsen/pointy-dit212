2053271a8f2f2bfdc60f3cc6a98b4a170193014c
Sgenne <simon.genne@gmail.com>
2020-10-06 13:58:58 +0200

    Comments to EntityFactory

--- project/src/main/java/game/services/EntityFactory.java
+++ project/src/main/java/game/services/EntityFactory.java
@@ -12,11 +12,15 @@ import game.model.entity.player.Player;
 import javafx.geometry.Point2D;
 
 public class EntityFactory {
+    private static final int BASEENEMYSTRENGTH = 5;
+
     private static final long BULLETFREQUENCY = GameLoop.SECOND;
-    private static final int ENEMYSTRENGTH = 5;
     private static final double BULLETRADIUS = 10;
     private static final double BULLETSPEED = 400;
     private static final int BULLETSTRENGTH = 1;
+
+    // Dictates how much increasing the difficulty increases the frequency of bullets. The higher the more frequent.
+    private static final double BULLETFREQUENCYFACTOR = 0.5;
 
     private static final double MISSILEMAXFORCE = 500;
     private static final double MISSILEMAXSPEED = 400;
@@ -24,6 +28,12 @@ public class EntityFactory {
     private static final int MISSILESTRENGTH = 3;
     private static final long MISSILEFREQEUNCY = GameLoop.SECOND * 2;
     private static final double MISSILERADIUS = 10;
+
+    // Dictates how much the difficulty changes the max force of the missiles.
+    private static final double MISSILEMAXFORCEFACTOR = 300;
+
+    // Dictates how much the difficulty changes the max speed of the missiles.
+    private static final double MISSILEMAXSPEEDFACTOR = 25;
 
 
     public static Player basicPlayer(double x, double y) {
@@ -44,9 +54,14 @@ public class EntityFactory {
     public static Enemy bulletEnemy(double x, double y, IEntity<?> target, int difficulty) {
         if (difficulty < 1) throw new IllegalArgumentException();
 
-        ShootBullet shootBullet = new ShootBullet((long ) (BULLETFREQUENCY / (difficulty * 0.5)), BULLETRADIUS, 0, BULLETSPEED, BULLETSTRENGTH);
-        Enemy enemy = basicEnemy(x, y, target, ENEMYSTRENGTH);
+        // Bullet frequency is impacted by given difficulty.
+        long bulletFrequency = (long ) (BULLETFREQUENCY / (difficulty * BULLETFREQUENCYFACTOR));
+
+        // Create Enemy with Ability ShootBullet.
+        ShootBullet shootBullet = new ShootBullet(bulletFrequency, BULLETRADIUS, 0, BULLETSPEED, BULLETSTRENGTH);
+        Enemy enemy = basicEnemy(x, y, target, BASEENEMYSTRENGTH);
         enemy.setAbilityBehaviour(new SingleAbilityBehavior(shootBullet));
+
         return enemy;
     }
 
@@ -56,10 +71,20 @@ public class EntityFactory {
 
     //Creates an enemy with the ability of homing missiles. The given difficulty changes the max speed and responsiveness of the missiles.
     public static Enemy missileEnemy(double x, double y, IEntity<?> target, int difficulty) {
+        if (difficulty < 1) throw new IllegalArgumentException();
+
+        // MovementBehaviour to be used by missiles fired by the Enemy.
         IMovementBehaviour movementBehaviour = new SeekingBehaviour();
-        ShootMissile shootMissile = new ShootMissile(MISSILEFREQEUNCY, MISSILERADIUS, MISSILEMAXFORCE + 300 * difficulty, MISSILEMAXSPEED + 25 * difficulty , MISSILEMINSPEED, MISSILESTRENGTH, movementBehaviour);
-        Enemy enemy = basicEnemy(x, y, target, ENEMYSTRENGTH);
+
+        // Max force and max speed of missile are impacted by difficulty.
+        double maxForce = MISSILEMAXFORCE + MISSILEMAXFORCEFACTOR * difficulty;
+        double maxSpeed = MISSILEMAXSPEED + MISSILEMAXSPEEDFACTOR * difficulty;
+
+        // Create Enemy with Ability ShootMissile.
+        ShootMissile shootMissile = new ShootMissile(MISSILEFREQEUNCY, MISSILERADIUS, maxForce, maxSpeed , MISSILEMINSPEED, MISSILESTRENGTH, movementBehaviour);
+        Enemy enemy = basicEnemy(x, y, target, BASEENEMYSTRENGTH);
         enemy.setAbilityBehaviour(new SingleAbilityBehavior(shootMissile));
+
         return enemy;
     }
 }
