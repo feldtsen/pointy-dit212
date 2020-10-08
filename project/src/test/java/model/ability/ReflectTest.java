aebefa5870d8a0274f51903d9691cc2c887b4e2a
Mattoom <kall0795@gmail.com>
2020-10-08 11:27:33 +0200

    reflect test

--- project/src/test/java/model/ability/ReflectTest.java
+++ project/src/test/java/model/ability/ReflectTest.java
@@ -11,6 +11,7 @@ import game.model.level.ILevel;
 import game.model.level.Level;
 import game.util.Utils;
 import javafx.geometry.Point2D;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -24,16 +25,19 @@ public class ReflectTest {
     IPlayer player;
     ILevel level;
     double control;
+    Point2D vector;
 
+    @Before
     public void init() {
         // Create ability reflect
         IAbility reflect = new Reflect(10, Math.PI / 2, 100, control, 0.1, 1000);
 
-        projectile = new Bullet(new Point2D(515, 500), 10, 10, 10, 1, new Point2D(50, 50));
         List<IProjectile<?>> projectiles = new ArrayList<>();
-        projectiles.add(projectile);
 
+        projectile = new Bullet(new Point2D(500, 500), 10, 10, 10, 1, new Point2D(50, 50));
+        level.getProjectiles().add(projectile);
         player = new Player(new Point2D(500, 500), 1, 1, 3, 0);
+
         player.addAbility(reflect);
         level = new Level(new ArrayList<>(), projectiles, new ArrayList<>(), player, 1200, 800);
 
@@ -48,15 +52,25 @@ public class ReflectTest {
     public void projectileInRangeZeroControl() {
 
         control = 0;
-        init();
-
+        level.getProjectiles().get(0).setPosition(new Point2D(515,500));
         Point2D bulletPosition = level.getProjectiles().get(0).getPosition();
         Point2D playerPosition = player.getPosition();
 
         // Vector between player and projectile
         Point2D vector = bulletPosition.subtract(playerPosition);
 
-        // Calculate unit vector
+        double dir = Utils.heading(vector);
+        player.getShape().setRotation(dir);
+
+        reflect();
+
+        Point2D newVelocity = newProjectileVelocity(vector);
+        assertEquals(newVelocity.getX(), level.getProjectiles().get(0).getVelocity().getX(),0.0);
+        assertEquals(newVelocity.getY(), level.getProjectiles().get(0).getVelocity().getY(), 0.0);
+    }
+
+    private Point2D newProjectileVelocity(Point2D vector){
+
         Point2D n = vector.normalize();
 
         // Vector representing the direction the player is facing
@@ -64,10 +78,6 @@ public class ReflectTest {
 
         Point2D newVelocityDirection = n.interpolate(f, control);
 
-        Point2D newVelocity = Utils.setMagnitude(newVelocityDirection, projectile.getVelocity().magnitude());
-        reflect();
-
-        assertEquals(newVelocity.getX(), level.getProjectiles().get(0).getVelocity().getX(),0.0);
-        assertEquals(newVelocity.getY(), level.getProjectiles().get(0).getVelocity().getY(), 0.0);
+        return Utils.setMagnitude(newVelocityDirection, projectile.getVelocity().magnitude());
     }
 }
