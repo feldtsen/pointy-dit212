f7997b18114d328f4fd62562243a335b71adee30
Sgenne <simon.genne@gmail.com>
2020-10-09 15:40:30 +0200

    Added ShootMissileTest

---
+++ project/src/test/java/model/ability/ShootMissileTest.java
@@ -0,0 +1,46 @@
+package model.ability;
+
+import game.model.ability.action.IAbilityAction;
+import game.model.entity.enemy.IEnemy;
+import game.model.entity.player.IPlayer;
+import game.model.entity.projectile.Missile;
+import game.model.level.ILevel;
+import game.model.level.Level;
+import game.services.EntityFactory;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ShootMissileTest {
+    private IEnemy user;
+    private IPlayer target;
+    private ILevel level;
+
+    private void init() {
+        target = EntityFactory.basicPlayer(100, 100);
+        user = EntityFactory.missileEnemy(0, 0, target, 1);
+
+        List<IEnemy> enemies = new ArrayList<>();
+        enemies.add(user);
+
+        level = new Level(enemies, new ArrayList<>(), new ArrayList<>(), target, 100000, 100000);
+    }
+
+    @Test
+    public void testCreateMissile() {
+        init();
+
+        // Initially the number of projectiles should be 0.
+        assertTrue(level.getProjectiles().size() == 0);
+
+        // Apply ability (create missile)
+        IAbilityAction abilityAction = user.applyAbility();
+        abilityAction.apply(level, 0);
+
+        // Test that a projectile gets added to level and that it is a missile.
+        assertTrue(level.getProjectiles().size() == 1 && level.getProjectiles().get(0) instanceof Missile);
+    }
+}
