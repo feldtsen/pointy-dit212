d2a60d10f0429980a17804e4c22d5b27e80c6b1b
Sgenne <simon.genne@gmail.com>
2020-10-14 11:14:18 +0200

    cleanup

--- project/src/test/java/model/movementBehaviour/FleeingBehvaiourTest.java
+++ project/src/test/java/model/movementBehaviour/FleeingBehvaiourTest.java
@@ -4,25 +4,18 @@ import game.model.entity.enemy.IEnemy;
 import game.model.entity.player.IPlayer;
 import game.services.EntityFactory;
 import javafx.geometry.Point2D;
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class FleeingBehvaiourTest {
-    /*TODO: test that placing an enemy too close to a target makes it move away.
-            test that placing an enemy too far away from a target makes it move closer.
-            test that placing an enemy in the "middle" makes it strafe
-     */
     private IEnemy enemy;
     private IPlayer player;
-
 
     private void setup(int enemyX, int enemyY) {
         player = EntityFactory.basicPlayer(0, 0);
         enemy = EntityFactory.bulletEnemy(enemyX, enemyY, player, 1);
     }
-
 
     @Test
     // Tests that an enemy, with the fleeing behaviour, will flee if too close to its target.
@@ -62,6 +55,7 @@ public class FleeingBehvaiourTest {
     @Test
     // If the enemy is neither too far away nor to close, it should be moving sideways.
     public void testStrafe() {
+
         // Places the enemy at a distance from the player where it should strafe.
         setup(400, 0);
 
@@ -69,7 +63,6 @@ public class FleeingBehvaiourTest {
         Point2D difVector = player.getPosition().subtract(enemy.getPosition());
 
         enemy.update(0.1, 1);
-
 
         // The enemy is strafing if its velocity vector is orthogonal to the original vector from the enemy to the player.
         assertTrue(difVector.dotProduct(enemy.getVelocity()) == 0);
